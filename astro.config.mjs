41d0a565851fd7fd3a09669bfca4dddcf60006f8
kirivanCode <ivansierravega8@gmail.com>
2025-05-11 10:28:47 -0500

    no se xdddd

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,32 +3,29 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import clerk from "@clerk/astro";
-import { esES }  from '@clerk/localizations';
+import { esES } from '@clerk/localizations';
 import { dark } from '@clerk/themes';
-
-
-
+import netlify from '@astrojs/netlify';
 import tailwindcss from '@tailwindcss/vite';
 
-import node from '@astrojs/node';
-
-// https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
 
-  integrations: [clerk({
-    localization: esES,
-    appearance:{
-      baseTheme: dark,
-    }
-  }), mdx(), sitemap()],
+  integrations: [
+    clerk({
+      localization: esES,
+      appearance: {
+        baseTheme: dark,
+      },
+    }),
+    mdx(),
+    sitemap()
+  ],
 
   vite: {
     plugins: [tailwindcss()],
   },
 
-  adapter: node({ mode: 'standalone' }),
-  output: 'server',
-
-  
+  adapter: netlify(),
+  output: 'server', // para SSR, puede omitirse si netlify lo establece automáticamente
 });
